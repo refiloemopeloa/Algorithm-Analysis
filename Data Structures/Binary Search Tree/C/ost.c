d61f67bee1b99dc03f8ef01a4fc82337d3a25ebb
Refiloe Mopeloa <72868391+refiloemopeloa@users.noreply.github.com>
2025-10-13 15:16:17 +0200

    Added compiler directives for running tests for bst vs ost

--- Data Structures/Binary Search Tree/C/ost.c
+++ Data Structures/Binary Search Tree/C/ost.c
@@ -4,10 +4,10 @@
 
 /**
  * @brief Initialize a new node with given parameters
- * 
+ *
  * Sets all fields of a Node structure to the specified values.
  * Initializes rank to 0 and visited flag to 0.
- * 
+ *
  * @param this The node to initialize
  * @param left Pointer to left child node
  * @param right Pointer to right child node
@@ -26,9 +26,9 @@ void new_Node(Node *this, Node *left, Node *right, Node *parent, int *key)
 
 /**
  * @brief Copy data from one node to another
- * 
+ *
  * Copies all data fields (except visited flag) from source node to destination node.
- * 
+ *
  * @param this Destination node to copy data to
  * @param other Source node to copy data from
  */
@@ -43,10 +43,10 @@ void copy_Node(Node *this, Node *other)
 
 /**
  * @brief Insert a new node into the binary search tree with rank maintenance
- * 
+ *
  * Inserts a new node while maintaining rank information for order statistics.
  * The rank represents subtree size. Updates ranks of all ancestor nodes during insertion.
- * 
+ *
  * @param this The binary search tree to insert into
  * @param new The node to be inserted into the tree
  */
@@ -56,7 +56,11 @@ void tree_insert(BST *this, Node *new)
     new->rank = 1;
     if (ptr == NULL)
     {
+#ifdef BST_TEST
         this->root = (Node *)malloc(sizeof(Node));
+#else
+        this->root = (Node *)arena_alloc(arena, sizeof(Node));
+#endif
         copy_Node(this->root, new);
         return;
     }
@@ -95,10 +99,10 @@ void tree_insert(BST *this, Node *new)
 
 /**
  * @brief Delete a node from the binary search tree with rank maintenance
- * 
+ *
  * Removes a node while maintaining rank information. Decrements ranks of all
  * ancestor nodes and frees the deleted node's memory.
- * 
+ *
  * @param this The binary search tree to delete from
  * @param z The node to be deleted from the tree
  */
@@ -131,14 +135,18 @@ void tree_delete(BST *this, Node *z)
         y->left = z->left;
         y->left->parent = y;
     }
+#ifdef BST_TEST
     free(z);
+#else
+    arena_free(arena, z);
+#endif
 }
 /**
  * @brief Find the i-th smallest element in the subtree (Order Statistics Select)
- * 
+ *
  * Implements the OS-SELECT algorithm to find the node with the i-th smallest key
  * in the subtree rooted at x. Uses rank information for efficient selection.
- * 
+ *
  * @param x The root node of the subtree to search
  * @param i The rank position to find (1-based index)
  * @return Node* The node with the i-th smallest key, or NULL if not found
@@ -171,10 +179,10 @@ Node *os_select(Node *x, int i)
 
 /**
  * @brief Determine the rank of a node in the tree (Order Statistics Rank)
- * 
+ *
  * Implements the OS-RANK algorithm to find the position of node x in the
  * sorted order of all keys in the tree. Returns -1 if node is NULL.
- * 
+ *
  * @param this The binary search tree containing the node
  * @param x The node to find the rank for
  * @return int The rank of the node (1-based position), or -1 if node is NULL
@@ -198,7 +206,8 @@ int os_rank(BST *this, Node *x)
         {
             if (y->parent->left != NULL)
                 r += y->parent->left->rank + 1;
-            else r++;
+            else
+                r++;
         }
         y = y->parent;
     }
@@ -225,7 +234,7 @@ int main()
 #elif BRANCH == 5
     int array[] = {7, 3, 1, 0, 2, 4, 6, 5, 9, 8};
 #elif BRANCH == 6
-    int array[] = {1,-1,-3,-4,-5,-2,0,3,2,4};
+    int array[] = {1, -1, -3, -4, -5, -2, 0, 3, 2, 4};
 #endif
     BST *tree = (BST *)malloc(sizeof(BST));
 
